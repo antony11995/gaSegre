2342fe437f4942a65d79bf359fba260046ba6e7c
AntonyMartinez <antony11995@hotmail.com>
2021-08-27 11:01:17 -0500

    Top 100 completado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from flaskext.mysql import MySQL
 
 app= Flask(__name__)
@@ -16,10 +16,19 @@ def index():
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
-
     cuentasGA = cursor.fetchall() 
     conn.commit()
     return render_template('/index.html',cuentasGA=cuentasGA)
+
+@app.route('/consultaporNombre/<int:id>')
+def consultaporNombre(id):
+    conn=mysql.connect()
+    cursor_name=conn.cursor()
+    cursor_name.execute("SELECT nombre FROM  `cuentas_ga` WHERE `id`=%s",id)
+    nombreCuenta = cursor_name.fetchall()  
+    conn.commit()
+    return nombreCuenta  
+
 
 @app.route('/consultaCuenta/<int:id>')
 def consultaCuenta(id):
@@ -29,25 +38,25 @@ def consultaCuenta(id):
     t1.`fecha`,t1.`duracion_sesion`,t1.`sesiones`,t2.`nombre`\
     FROM sesiones_ga t1 INNER JOIN `cuentas_ga` t2 \
     ON t1.id_cuenta = t2.id WHERE t2.id=%s   GROUP BY sesiones DESC",id)
-    # cursor.execute("SELECT `fecha`,\
-    # `duracion_sesion`,`sesiones` FROM sesiones_ga \
-    # WHERE id_cuenta=%s ORDER BY sesiones DESC; SELECT `nombre` FROM `cuentas_ga` WHERE id=%s",datos)
     registrosCuenta = cursor.fetchall() 
     conn.commit()  
-    cursor_name=conn.cursor()
-    cursor_name.execute("SELECT nombre FROM  `cuentas_ga` WHERE `id`=%s",id)
-    
-    nombreCuenta = cursor_name.fetchall()  
-    conn.commit()
-    print(str(nombreCuenta))
+    nombreCuenta=consultaporNombre(id)
     return render_template('/consultaCuenta.html',registrosCuenta=registrosCuenta,nombreCuenta=nombreCuenta)
-    
+
+ 
 
 
-@app.route('/top100')
-def top100():
-
-   return render_template('/top100.html')
+@app.route('/top100/<int:id>')
+def top100(id):
+    sql="SELECT fecha, duracion_sesion, sesiones FROM sesiones_ga WHERE id_cuenta=%s ORDER BY sesiones DESC LIMIT 100;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,id)
+    registrosTop100 = cursor.fetchall() 
+    conn.commit()
+    nombreCuenta=consultaporNombre(id)    
+    return render_template('/top100.html',registrosTop100=registrosTop100,nombreCuenta=nombreCuenta)
+   
 
 if __name__=='__main__':
     app.run(debug=True)
